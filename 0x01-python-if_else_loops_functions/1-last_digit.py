57e854267a5494ce43ee231b5c6e78332d052eca
MazenAtlam <mazenatef5510@gmail.com>
2023-10-04 01:29:28 -0700

    task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = -98# number = random.randint(-10000, 10000)
+number = random.randint(-10000, 10000)
 if number < 0:
     ldigit = -1 * ((number * -1) % 10)
 else:
