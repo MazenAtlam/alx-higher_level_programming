f3c1ebe19f6a2950da9b1b3e3215ab7204e91c76
MazenAtlam <mazenatef5510@gmail.com>
2023-10-23 15:52:05 -0700

    advanced, 1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as err:
-        print("Exception:", err, file=sys.stderr)
+        print("Exception: ", err, file=sys.stderr)
         return False
     else:
         return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-        result = fct(args)
+        result = fct(*args)
     except Exception as err:
         print("Exception: ", err, file=sys.stderr)
         return None
