398b03b14922f5117985fa83eebce9fb9f33bb16
MazenAtlam <mazenatef5510@gmail.com>
2024-01-31 23:13:41 +0000

    task6, pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,7 +63,7 @@ it will raise a TypeError with massage
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) > 2 or \
+        if type(value) is not tuple or len(value) is not 2 or \
                 type(value[0]) is not int or value[0] < 0 or \
                 type(value[1]) is not int or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
@@ -91,7 +91,7 @@ Returns: Nothing
 """
 
         if self.__size is 0:
-                print()
+            print()
         else:
             for i in range(self.__position[1]):
                 print()
