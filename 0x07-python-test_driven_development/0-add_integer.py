20cefa2eafc38cfa225978793d1bfd1316127184
MazenAtlam <mazenatef5510@gmail.com>
2024-02-07 20:33:39 -0800

    task0, 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,6 +9,7 @@
 
 def add_integer(a, b=98):
     """A function that adds two integers
+    Floats are casted to integers
 
     Args:
         @a: first value
@@ -25,3 +26,9 @@ def add_integer(a, b=98):
         raise TypeError('b must be an integer')
 
     return int(a) + int(b)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testfile(tests/0-add_integer.txt)
