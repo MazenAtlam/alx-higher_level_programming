9a0142c3637a251e7323c674da16aa0a119e3629
Mazen Atef <mazenatef5510@gmail.com>
2024-05-09 00:49:51 +0300

    task1 v1.0

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""This module have a function used to write a string to a text file (UTF8)
+"""
+
+
+def write_file(filename="", text=""):
+    """A function that writes a string to a text file (UTF8)
+    and returns the number of characters written
+
+    Args:
+        filename (str, optional): The name of the text file. Defaults to "".
+        text (str, optional): The string to be written. Defaults to "".
+    """
+    with open(filename, mode='w', encoding="utf-8") as file:
+        no_of_chars = file.write(text)
+
+    return no_of_chars
