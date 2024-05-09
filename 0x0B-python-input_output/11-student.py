02e46803396d7675ecf4af2a76ae1fe18668d734
Mazen Atef <mazenatef5510@gmail.com>
2024-05-09 06:10:23 +0300

    Task 11 v1.0

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""This module has a class Student that defines a student
+"""
+
+
+class Student:
+    """A class Student that defines a student
+
+    Attributes:
+        first_name (string): The student first name
+        last_name (string): The student last name
+        age (int): The student age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Intializing instance attributes
+
+        Args:
+            first_name (string): The student first name
+            last_name (string): The student last name
+            age (int): The student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """A method that retrievesa dictionary representation
+        of a Student instance
+
+        - If attrs is a list of strings,
+            only attribute names contained in this list must be retrieved.
+        - Otherwise, all attributes must be retrieved
+
+        Returns:
+            dict: a dictionary representation of a Student instance
+        """
+        attrs_dict = dict()
+        if attrs is None or isinstance(attrs, list) is False:
+            return self.__dict__
+
+        for element in attrs:
+            if not isinstance(element, str):
+                return self.__dict__
+
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                attrs_dict.update({key: value})
+        return attrs_dict
+
+    def reload_from_json(self, json):
+        """A function that replaces all attributes of the Student instance
+
+        Args:
+            json (dict): A dictonary of the new values of the attributes
+        """
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
