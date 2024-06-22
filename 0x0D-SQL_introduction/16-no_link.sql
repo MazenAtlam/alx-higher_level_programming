0f9647b0a60ca7debe01e99cac69e3756e2fe6f5
MazenAtlam <mazenatef5510@gmail.com>
2024-06-23 00:53:05 +0300

    Task [11 | 16] version 1.1

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- A script that lists all records
 -- of the table second_table of the database hbtn_0c_0
-SELECT score, name
-ORDER BY score DESC
-WHERE name IS NOT NULL;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
