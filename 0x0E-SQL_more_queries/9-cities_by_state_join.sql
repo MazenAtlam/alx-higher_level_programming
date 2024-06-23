95ae31709462e7b28ae44977f92f4a41089fdd67
MazenAtlam <mazenatef5510@gmail.com>
2024-06-23 11:36:33 +0300

    Task 9 version 1.1

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa\
-SELECT c.id AS id, c.name AS name, s.name AS name
+SELECT c.id, c.name, s.name
 FROM hbtn_0d_usa.cities AS c
 LEFT JOIN hbtn_0d_usa.states AS s
 ON c.state_id = s.id
