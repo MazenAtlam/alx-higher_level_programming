4af772d343a3cdaad5f1d3c697f0207a94e9da9b
MazenAtlam <mazenatef5510@gmail.com>
2024-06-23 11:45:57 +0300

    Task 10 version 2.0

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- have at least one genre linked
 SELECT s.title AS title, g.genre_id AS genre_id
 FROM tv_shows AS s
-LEFT JOIN tv_show_genres AS g
+INNER JOIN tv_show_genres AS g
 ON s.id = g.show_id
 ORDER BY s.title, g.genre_id;
