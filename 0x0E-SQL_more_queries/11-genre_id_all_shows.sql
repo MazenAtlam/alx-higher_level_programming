56379b9e0792a708b6c3052dfeb4b1ef2c7b6e1e
MazenAtlam <mazenatef5510@gmail.com>
2024-06-23 11:47:32 +0300

    Task 11 version 1.2

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- A script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT s.title AS title, g.genre_id AS genre_id
+SELECT s.title, g.genre_id
 FROM tv_shows AS s
 JOIN tv_show_genres AS g
 ON s.id = g.show_id
