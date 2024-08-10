4c8a9436e174b5a192e179b82df4ccea4231ae0b
MazenAtlam <mazenatef5510@gmail.com>
2024-08-09 21:39:01 -0700

    Submit task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+// A script that reads and prints the content of a file
+
+// Create file stream
+const fs = require('fs')
+
+// Read the cmd line
+const args = process.argv
+
+// Get file name
+const file = args[2]
+
+if (file != undefined)
+{
+  // Read data from the file
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      // Error reading the file
+      console.log(err)
+    } else {
+      // Print the content of the file
+      console.log(data)
+    }
+  })
+} else {
+	console.log('Usage: ./0-readme.js file_path')
+}
